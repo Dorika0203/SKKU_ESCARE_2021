fd15ba3b6ed9cfb260bf97d26630c248728c7a38
min-swo <msms0226@g.skku.edu>
2021-09-24 22:56:24 +0900

    AccountNumber rule set

--- src/main/java/com/example/demo/repository/AccountDataRepository.java
+++ src/main/java/com/example/demo/repository/AccountDataRepository.java
@@ -6,9 +6,10 @@ import com.example.demo.model.AccountDataModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountDataRepository extends JpaRepository<AccountDataModel, Long> {
-    AccountDataModel findByUserId (String userId);
-
-    Boolean existsByUserId (String userId);
 
     List<AccountDataModel> findAllByUserId(String userID);
+
+    boolean existsByAccount(String account);
+
+    AccountDataModel findByAccount(String account);
 }
